d894cd42be6ebd074f1f108cf67e50d9c6514049
Miguel Afonso <miguel.afonso@mercedes-benz.io>
2023-10-29 11:34:58 +0000

    Tarifa Segura

--- ezu.py
+++ ezu.py
@@ -3,30 +3,31 @@ from datetime import datetime, time, timedelta
 
 import main
 
-tarifasegura_tecto_simples = 0.1329
-tarifasegura_chao_simples = 0.0729
-tar = -0.0121
-
-
-def calc_cgs(row):
-    cgs = (row['encargos_regulacao'] + row['encargos_restricoes_pdbf'] + row['encargos_restricoes_tempo_real'] + row[
-        'desvio_ren']) / row['consumo_global']
-    return cgs
+tarifasegura_tecto_simples = 140
+tarifasegura_chao_simples = 80
+tar = -0.0121 * 1000
 
 
 def calc_perdas(row):
     perdas = (1 + row['perda_atrt']) * (1 + row['perda_at']) * (1 + row['perda_mt']) * (1 + row['perda_bt'])
     return perdas
 
+def calc_cgs(row):
+    cgs = (row['encargos_regulacao'] + row['encargos_restricoes_pdbf'] + row['encargos_restricoes_tempo_real'] + row[
+        'desvio_ren']) / row['consumo_global']
+    return cgs
+
 def calc_price(row):
-    price = (row['preco_omie'] + calc_cgs(row) + row['k']) * calc_perdas(row)
-    return price.clip(lower=tarifasegura_chao_simples*1000, upper=tarifasegura_tecto_simples*1000)
+    omie = row['preco_omie'] #.clip(lower=tarifasegura_chao_simples, upper=tarifasegura_tecto_simples)
+    price = (omie + calc_cgs(row) + row['k']) * calc_perdas(row) + tar
+    return price
 
 def calc_cost(row):
-    cost = calc_price(row) * row['curva_carga'] + (tar * row['curva_carga'] * 1000)
+    cost = calc_price(row) * row['curva_carga']
     return cost
 
-ezu = pd.read_csv('data/ezu/20231002173951_2023-08-30_2023-09-29_32083.csv', encoding="ISO-8859-1", skiprows=3,
+
+ezu = pd.read_csv('data/ezu/20230927132640_2023-07-30_2023-08-06_12448.csv', encoding="ISO-8859-1", skiprows=3,
                   skipfooter=9, delimiter=';', quotechar='"', engine='python', decimal=',')
 
 ezu_df = pd.DataFrame(
@@ -34,7 +35,6 @@ ezu_df = pd.DataFrame(
              'consumo_global', 'perda_bt', 'perda_mt', 'perda_at', 'perda_atrt', 'preco_omie', 'k', 'curva_carga', ],
     index=pd.to_datetime([]))
 
-#print(ezu_df.info())
 
 for index, row in ezu.iterrows():
     date = datetime.strptime(row['Data PT'], '%Y-%m-%d')
@@ -65,18 +65,18 @@ for index, row in ezu.iterrows():
     ezu_df.loc[date] = [encargos_regulacao, encargos_restricoes_pdbf, encargos_restricoes_tempo_real, desvio_ren,
                         consumo_global, perda_bt, perda_mt, perda_ar, perda_atrt, preco_omie, k, cuva_carga]
 
-# print(ezu_df.info())
-# print(ezu_df.describe())
-# print(ezu_df.head())
-
-# print(calc_perdas(ezu_df.loc['2023-07-30 00:00':'2023-07-30 02:00']))
 
 
-ezu_slice = ezu_df.loc['2023-08-30 00:00':'2023-09-30 00:00']
+#ezu_slice = ezu_df.loc['2023-07-30 00:00':'2023-08-07 00:00']
+ezu_slice = ezu_df
+
+
+ezu_slice['price'] = calc_price(ezu_slice)
+ezu_slice['cost'] = calc_cost(ezu_slice)
 
 print("Consumo :", '{:.0f}'.format(ezu_slice.curva_carga.sum()*1000))
-print("Custo :", '{:.2f}'.format(calc_cost(ezu_slice).sum()))
+print("Custo :", '{:.5f}'.format(calc_cost(ezu_slice).sum()))
 print("Preço Medio kWh :", '{:.6f}'.format(calc_cost(ezu_slice).sum() / ezu_slice.curva_carga.sum() / 1000))
 
 
-# convert the above to golang
+#print(ezu_slice.head(70)[['price', 'cost']])
